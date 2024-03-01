b3434cb742f5f8504a70b2304a2c0fbf02fabb33
Peter <52469803+Frozenbitz@users.noreply.github.com>
2024-03-01 15:42:01 +0100

    test

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@ int main() {
     crow::SimpleApp app;
 
     // Route: GET Request on Root '/'
-    CROW_ROUTE(app, "/")([](){
+    CROW_ROUTE(app, "/ot")([](){
         return "Welcome to the Crow C++ Web Server!";
     });
 
     // Route: POST Request on '/submit'
-    CROW_ROUTE(app, "/submit").methods("POST"_method)([](const crow::request& req){
+    CROW_ROUTE(app, "/ot/submit").methods("POST"_method)([](const crow::request& req){
         auto x = crow::json::load(req.body);
         if (!x)
             return crow::response(400);
@@ -18,7 +18,7 @@ int main() {
     });
 
     // Route: GET Request with URL Parameters '/params/<int>/<string>'
-    CROW_ROUTE(app, "/params/<int>/<string>")([](int int_param, std::string str_param){
+    CROW_ROUTE(app, "/ot/params/<int>/<string>")([](int int_param, std::string str_param){
         std::ostringstream os;
         os << "Int: " << int_param << ", String: " << str_param;
         return crow::response{os.str()};
